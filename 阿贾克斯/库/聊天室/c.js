f9bb8d5de27fa489e8baa1508c02d9f74854e9fe
louli97 <72250251+louli97@users.noreply.github.com>
2021-02-04 12:19:17 +0800

    Add files via upload

--- 阿贾克斯/库/聊天室/c.js
+++ 阿贾克斯/库/聊天室/c.js
@@ -1,1 +1,15 @@
-
+function setcookie(name,value)
+{
+var Days = 30;
+var exp = new Date();
+exp.setTime(exp.getTime() + Days*24*60*60*1000);
+document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+function getcookie(name)
+{
+var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+if(arr=document.cookie.match(reg))
+return unescape(arr[2]);
+else
+return null;
+}
